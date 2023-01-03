9b10c39217f4a40d716c152f9ec01fcd5a37b954
mkmak2 <112261097+mkmak2@users.noreply.github.com>
2023-01-03 17:27:31 +0100

    Delete investment-app-front/components directory

--- investment-app-front/components/SidePanel/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledSidePanel = styled.div`
-    width: 200px;
-    height: 500px;
-    border-right: 4px solid #BF9742;
-    margin-top: 100px;
-`;
