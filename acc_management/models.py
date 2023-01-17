d44e76b159870ce95852a1366906926d2c564dad
unknown <mkmak.code@gmail.com>
2023-01-17 14:50:02 +0100

    feat: form walidation

--- acc_management/models.py
+++ acc_management/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserData(models.Model):
  username = models.CharField(max_length=255, unique=True, primary_key=True)
  password = models.CharField(max_length=255)
- email = models.EmailField(max_length=255)
+ email = models.EmailField(max_length=255,blank=True)
  is_logged = models.BooleanField(default=False)
 
  def __str__(self):
--- investment-app-front/components/LoginForm/LoginForm.js
+++ investment-app-front/components/LoginForm/LoginForm.js
@@ -6,20 +6,35 @@ import { useState } from 'react';
 const LoginForm = () => {
 
     const [type, setType] = useState(true);
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
 
     const handleClick = (e) =>{
         e.preventDefault();
+        const usernameToSend = username;
+        const passwordToSend = password;
+
+        //metoda do wysyłania
     }
     
     const display = type ? 'password' : 'text';
     const color = type ? 'grey' : ' #E6B325';
 
+    const handleUserChange = e => {
+        setUsername(e.target.value)
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+
+
     return ( 
         <div>
             <h1>Sing in</h1>
         <StyledForm color={color}>
-            <StyledInput type='text' placeholder='Login...' />
-            <StyledInput type={display} placeholder='Password...' />
+            <StyledInput type='text' placeholder='Login...' value={username} onChange={handleUserChange} />
+            <StyledInput type={display} placeholder='Password...' value={password} onChange={handlePasswordChange}/>
             <div className="show">
             <FontAwesomeIcon icon={faEye} id='icon' onClick={() => setType(!type)}/>
             </div>
