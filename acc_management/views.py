3859769d44e7cf270430c49178aca20fb6595212
Dawid511 <da.gra1@wp.pl>
2023-01-17 17:07:24 +0100

    dodany warunek do logowania, pole do przechowywania inwestycji

--- acc_management/views.py
+++ acc_management/views.py
@@ -29,6 +29,8 @@ def userList(request):
 
     return Response(serializer.data)
 
+
+
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def userDetail(request, pk):
@@ -42,6 +44,7 @@ def userDetail(request, pk):
 def userCreate(request):
     name = request.data.get('username')
     serializer = UserDataSerializer(data=request.data)
+    
     if not(UserData.objects.filter(username = name).exists()):
          if serializer.is_valid():
             serializer.save()
@@ -52,17 +55,26 @@ def userCreate(request):
     
    
 
-@api_view(['POST'])     #update
+#czy istnieje 303, czy haslo 304
+@api_view(['POST'])     #update, raczej tylko do logowania, dopisania inwestycji
 @permission_classes((permissions.AllowAny,))
 def userUpdate(request, pk):
     lookup_field="username"
     users = UserData.objects.get(username=pk)
+    name = request.data.get('username')
+    passw = request.data.get('password')
     serializer = UserDataSerializer(instance=users, data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
+    if not(UserData.objects.filter(username = name).exists()):
+        return HttpResponse(content="", status=303)
+    elif passw == users.password:
+         if serializer.is_valid():
+            serializer.save()
         
-    return Response(serializer.data)
+         return Response(serializer.data)
+    else:
+        return HttpResponse(content="", status=304)
+        
 
 @api_view(['DELETE'])     #delete
 @permission_classes((permissions.AllowAny,))
