ed08ef20eb982aef199a647e107adbcf15a731b2
kkanie <kaniepien2@gmail.com>
2023-01-19 00:32:16 +0100

    usuniecie niepotrzebnych rzeczy

--- api/form.py
+++ api/form.py
@@ -1,12 +1,7 @@
 from django.forms import ModelForm
 from django import forms
-from .models import StockInfo, StockInfoA
+from .models import StockInfoA
 
-class UploadForm(ModelForm):
-    symbol = forms.TextInput()
-    class Meta:
-        model = StockInfo
-        fields = ['data']
 
 class UploadForm2(ModelForm):
     symbol = forms.TextInput()
